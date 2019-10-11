2a741c566442597476cf03671445c906e209e5ec
Frederic Bregier <frederic.bregier@waarp.fr>
2019-10-11 11:59:24 +0200

    Initial GH pages

--- WaarpVitamInstall/waarp/bin/r66/InitR66Database.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-java -cp %ROOT%/lib/r66/%WaarpVitam% -Dlogback.configurationFile=%ROOT%/conf/r66/logback.xml org.waarp.openr66.server.ServerInitDatabase %ROOT%/conf/r66/config-serverInitA.xml  -initdb -dir %ROOT%/data/r66/conf -auth %ROOT%/conf/r66/OpenR66-authent-A.xml
--- WaarpVitamInstall/waarp/bin/r66/OperationCheck.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-if [ $# -eq 0 ]
-then
-  echo $0 Need at least file argument as /waarp/data/r66/XxxFactory/XxxRequest.xxx.json
-  exit 1
-fi
-
-java -cp %ROOT%/lib/r66/%WaarpVitam% -Dlogback.configurationFile=%ROOT%/conf/r66/logback-client.xml -Dvitam.tmp.folder=%ROOT%/tmp/r66 -Dvitam.config.folder=%ROOT%/conf/r66/vitam -Dvitam.data.folder=%ROOT%/data/r66 -Dvitam.log.folder=%ROOT%/log/r66 org.waarp.vitam.common.OperationCheck $1
